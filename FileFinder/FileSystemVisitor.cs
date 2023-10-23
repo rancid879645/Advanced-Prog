3179b1759420e2b044d9877794df9c69dfd4c165
Jonathan Velosa <jonathan_velosa@epam.com>
2023-10-23 14:07:45 -0500

    First version

---
+++ FileFinder/FileSystemVisitor.cs
@@ -0,0 +1,54 @@
+﻿namespace FileFinder
+{
+    public class FileSystemVisitor
+    {
+        private readonly string _rootPath;
+        public delegate void SearchStartedEventHandler(object sender, EventArgs e);
+        public delegate void SearchFinishedEventHandler(object sender, EventArgs e);
+
+        public FileSystemVisitor(string rootPath)
+        {
+            this._rootPath = rootPath;
+        }
+
+        public event SearchStartedEventHandler SearchStarted;
+        public event SearchFinishedEventHandler SearchFinished;
+
+        public IEnumerable<string> FindFile(string fileName = "")
+        {
+            var fullTree = GetFullTree();
+            OnSearchStarted();
+            foreach (var directory in fullTree.Where(directory => directory.ToUpper().Contains(fileName.ToUpper())))
+            {
+                yield return directory;
+            }
+            OnSearchFinished();
+        }
+
+        public void PrintTree(IEnumerable<string> directories)
+        {
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+        }
+
+        public List<string> GetFullTree()
+        {
+            return Directory.GetFileSystemEntries(_rootPath, "*", SearchOption.AllDirectories).ToList();
+        }
+
+        protected virtual void OnSearchStarted()
+        {
+            SearchStarted?.Invoke(this,EventArgs.Empty);
+        }
+
+        protected virtual void OnSearchFinished()
+        {
+            SearchFinished?.Invoke(this,EventArgs.Empty);
+        }
+
+
+
+    }
+}
---
+++ FileFinder/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FileFinder;
+Console.WriteLine("Root path:");
+var rootPath = Console.ReadLine();
+if (rootPath is not (null or ""))
+{
+    var fileVisitor = new FileSystemVisitor(rootPath);
+
+    fileVisitor.SearchStarted += (sender, e) => Console.WriteLine(" ******The search Begins (⌐■_■) \n");
+    fileVisitor.SearchFinished += (sender, e) => Console.WriteLine("\n ******The search finished └(^o^ )Ｘ( ^o^)┘");
+
+    Console.WriteLine("-------------------------------------");
+    fileVisitor.PrintTree(fileVisitor.GetFullTree());
+    Console.WriteLine("-------------------------------------");
+    Console.WriteLine("Search:");
+    var fileName = Console.ReadLine();
+    if (fileName != null)
+    {
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("Result: \n"); 
+        fileVisitor.PrintTree(fileVisitor.FindFile(fileName));
+        Console.WriteLine("-------------------------------------");
+    }
+}
